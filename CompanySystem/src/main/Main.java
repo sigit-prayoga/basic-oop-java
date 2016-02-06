148a745ed013d1b64eb3fce8f3cb46fade8b8a60
Sigit Prayoga <prayoga.sigit@gmail.com>
2016-02-05 20:32:35 -0600

    improve main function

--- CompanySystem/src/main/Main.java
+++ CompanySystem/src/main/Main.java
@@ -2,12 +2,35 @@ package main;
 import invetories.Iphone;
 import invetories.Laptop;
 import invetories.SamsungGalaxy;
+import people.Employee;
 import people.QAEngineer;
+import people.SoftwareEngineer;
 
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		createDevTeam();
+		
+	}
+	
+	public static void createDevTeam(){
+		SoftwareEngineer se = new SoftwareEngineer("John", "01");
+		QAEngineer qa = new QAEngineer("Lisa", "02", 30);
+		Employee pm = new Employee("David", "03");
+		
+	}
+	
+	public static void createCusterServiceRep(){
+		Iphone iphone = new Iphone();
+		SamsungGalaxy samsung = new SamsungGalaxy();
+		
+		QAEngineer brian = new QAEngineer("Mark", "ID1", 32);
+		brian.callCustomer(iphone, "1", samsung);
+	}
+	
+	public static void buildLaptop(){
 		Laptop asus = new Laptop("Asus", 5000000);
 		asus.getName();
 		asus.getPrice();
@@ -17,11 +40,5 @@ public class Main {
 		
 		isRunning =  asus.runApplication("photoshop");
 		System.out.println("Is the app running? "+isRunning);
-		
-		Iphone iphone = new Iphone();
-		SamsungGalaxy samsung = new SamsungGalaxy();
-		
-		QAEngineer brian = new QAEngineer("Mark", "ID1", 32);
-		brian.callCustomer(iphone, "1", samsung);
 	}
 }
